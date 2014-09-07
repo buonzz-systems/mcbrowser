e4eaf1d05faee65504a5c0da50396b7be0992984
buonzz <buonzz@gmail.com>
2014-09-07 10:01:23 +0800

    added expire field

--- add_key.php
+++ add_key.php
@@ -23,7 +23,12 @@ require "init.php";
             <div class="form-group">
               <label for="txtValue">Value</label>
               <textarea class="form-control" id="txtValue" rows="3"  name="txtValue" placeholder="Enter the value in here"></textarea>
-            </div>                        
+            </div>
+             <div class="form-group">
+              <label for="txtExpire">Expire</label>
+              <input type="text" class="form-control" name="txtExpire" id="txtExpire" placeholder="Enter the Expiration Value" value="0">
+              <span class="help-block">Number of seconds since January 1, 1970, as an integer or a number of seconds starting from current time. In the latter case, this number of seconds may not exceed 60*60*24*30 (number of seconds in 30 days); if the expiration value is larger than that, the server will consider it to be real Unix time value rather than an offset from current time</span>
+            </div>                       
             <button type="submit" class="btn btn-default">Save</button>
           </form>                   
       </div>
