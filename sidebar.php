205dd2fa8f30f6c921a7156b3966bb7d7674603b
buonzz <buonzz@gmail.com>
2014-09-07 08:51:08 +0800

    highlight selected key in the sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,8 @@
+<?php 
+
+$selected_key = isset($_GET['key']) ? $_GET['key'] : '';
+
+?>
 <div class="col-sm-3 col-md-2 sidebar">
 <ul class="nav nav-sidebar">
 <?php
@@ -5,7 +10,12 @@ if(count($keys)>0)
 {
   foreach($keys as $k)
   {
-    echo '<li><a href="show_key.php?key='.$k.'">'.$k.'</a></li>';
+  	if($selected_key == $k)
+    	echo '<li class="active"><a href="show_key.php?key='.$k.'">'.$k.'</a></li>';
+    elseif($selected_key == '')
+    	echo '<li><a href="show_key.php?key='.$k.'">'.$k.'</a></li>';
+   	else
+   		echo '<li><a href="show_key.php?key='.$k.'">'.$k.'</a></li>';
   }
 }
 else
