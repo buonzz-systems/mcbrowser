c9c7558c3df2cec2d201e8cae5f84fc82c03b9ec
buonzz <buonzz@gmail.com>
2014-09-07 08:15:36 +0800

    added changes in header

--- header_menu.php
+++ header_menu.php
@@ -10,10 +10,7 @@
   </div>
   <div class="navbar-collapse collapse">
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="#">Dashboard</a></li>
-      <li><a href="#">Settings</a></li>
-      <li><a href="#">Profile</a></li>
-      <li><a href="#">Help</a></li>
+      <li><a href="#">Create new Entry</a></li>      
     </ul>
     <form class="navbar-form navbar-right">
       <input type="text" class="form-control" placeholder="Search...">
